0f50c32a87cd4014e5ec036a30c3c0ef96037594
Ivan Panagushin <hellpanda44@gmail.com>
2024-01-03 18:02:35 +0300

    added new migration

--- src/StaffNook.Infrastructure/Migrations/20231112193817_Init.Designer.cs
+++ src/StaffNook.Infrastructure/Migrations/20231112193817_Init.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StaffNook.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using StaffNook.Infrastructure.Persistence;
 namespace StaffNook.Infrastructure.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231112193817_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
